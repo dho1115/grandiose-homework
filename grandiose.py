0580a901d57776bfbee1f661836f7e680ae37792
admin_dho <davidhorph@hotmail.com>
2024-12-10 16:06:11 -0600

    updates.

--- grandiose.py
+++ grandiose.py
@@ -8,16 +8,16 @@ def FindSynonym(word="operate"):
    load_dotenv();
    url = "https://thesaurus-by-api-ninjas.p.rapidapi.com/v1/thesaurus"
 
-   querystring = dict(word=word);
+   params = dict(word=word);
 
    headers = {
       "x-rapidapi-key": getenv("myapikey"), #please subscribe to the RAPID URL. Once inside, find the url above to get your own API key.
       "x-rapidapi-host": getenv("myapihost") #also subscribe to get the host.
    }
 
-   response = get(url, headers=headers, params=querystring)
+   response = get(url, params, headers=headers);
 
    return choice(response.json()["synonyms"]);
 
-def result(fn, synonym):
-   return f"My laptop is so much {synonym} I'm surprised yours can {fn}!!!"
+def result(randomWord, synonym):
+   return f"My laptop is so much {synonym} I'm surprised yours can {randomWord}!!!"
--- grandiose_main.py
+++ grandiose_main.py
@@ -1,5 +1,5 @@
 from grandiose import FindSynonym, result;
 
 if __name__ == '__main__':
-   SynonymInput = input("Input a synonym for \"faster\".");
+   SynonymInput = input("Input a synonym for \"faster\": ");
    print(result(FindSynonym("function"), SynonymInput))
